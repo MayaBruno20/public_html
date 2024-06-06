bdad3b6c59633334a3c5b0621577126132b1410a
mayabruno20 <maia.contato17@outlook.com>
2024-06-06 14:50:45 -0300

    modified:   .gitignore 	modified:   cypress/e2e/1-getting-started/navigation_spec.cy.js 	modified:   nav/contato.html

--- cypress/e2e/1-getting-started/navigation_spec.cy.js
+++ cypress/e2e/1-getting-started/navigation_spec.cy.js
@@ -1,4 +1,5 @@
 // cypress/e2e/navigation_spec.js
+// cypress/e2e/navigation_spec.js
 
 describe('Navigation Buttons', () => {
     beforeEach(() => {
@@ -7,25 +8,21 @@ describe('Navigation Buttons', () => {
 
     it('Scroll to Email Section', () => {
         cy.get('button[data-target="#email"]').click();
-        cy.url().should('include', '#email');
         cy.get('#email').should('be.visible');
     });
 
     it('Scroll to Filiais Section', () => {
         cy.get('button[data-target="#filiais"]').click();
-        cy.url().should('include', '#filiais');
         cy.get('#filiais').should('be.visible');
     });
 
     it('Scroll to Ligações Section', () => {
         cy.get('button[data-target="#ligacoes"]').click();
-        cy.url().should('include', '#ligacoes');
         cy.get('#ligacoes').should('be.visible');
     });
 
     it('Scroll to WhatsApp Section', () => {
         cy.get('button[data-target="#whatsapp"]').click();
-        cy.url().should('include', '#whatsapp');
         cy.get('#whatsapp').should('be.visible');
     });
 });
